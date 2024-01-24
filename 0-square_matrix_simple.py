a3a5a8171e98c2562652669685de88b7d586e200
dohaMST <mastourdoha.dev001@gmail.com>
2024-01-24 06:27:17 -0800

    task0 v2

--- 0-square_matrix_simple.py
+++ 0-square_matrix_simple.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    arr = []
-    for x in matrix:
-        arr.append([i**2 for i in x])
-    return arr
+    new_matrix = matrix.copy()
 
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+
+    return (new_matrix)
