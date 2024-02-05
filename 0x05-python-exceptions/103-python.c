0e1bb21efac1ac2bb64752de6305e060d729901a
dohaMST <mastourdoha.dev001@gmail.com>
2024-02-05 15:08:49 -0800

    ta

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,8 +1,5 @@
 #include <Python.h>
 
-void print_python_list(PyObject *p);
-void print_python_bytes(PyObject *p);
-void print_python_float(PyObject *p);
 /**
  * print_python_list - a function tha prints info about Py lists
  * @p: list object
