59813a8b56bbac29c5261a954bf1ee561754e3a9
dohaMST <mastourdoha.dev001@gmail.com>
2024-02-06 15:44:34 -0800

    task8

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
 
-"""Define a class Square."""
+"""defining of a class square."""
 
 
 class Square:
-    """Represent a square."""
+    """representation of a square."""
 
     def __init__(self, size=0):
-        """Initialize a new square.
-
-        Args:
-            size (int): The size of the new square.
-        """
         self.size = size
 
     @property
     def size(self):
-        """Get/set the current size of the square."""
         return (self.__size)
 
     @size.setter
@@ -28,29 +22,22 @@ class Square:
         self.__size = value
 
     def area(self):
-        """Return the current area of the square."""
         return (self.__size * self.__size)
 
     def __eq__(self, other):
-        """Define the == comparision to a Square."""
         return self.area() == other.area()
 
     def __ne__(self, other):
-        """Define the != comparison to a Square."""
         return self.area() != other.area()
 
     def __lt__(self, other):
-        """Define the < comparison to a Square."""
         return self.area() < other.area()
 
     def __le__(self, other):
-        """Define the <= comparison to a Square."""
         return self.area() <= other.area()
 
     def __gt__(self, other):
-        """Define the > comparison to a Square."""
         return self.area() > other.area()
 
     def __ge__(self, other):
-        """Define the >= compmarison to a Square."""
         return self.area() >= other.area()
