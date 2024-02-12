a09e080b08f7b555f081e2848bfa43f0b9147ef7
dohaMST <mastourdoha.dev001@gmail.com>
2024-02-12 22:38:39 +0100

    task7

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""defining the rectangle class"""
+
+
+class Rectangle:
+    """representing the rectangle"""
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.height * self.width
+
+    def perimeter(self):
+        if (self.height != 0 and self.width != 0):
+            return 2 * (self.height + self.width)
+        return 0
+
+    def __str__(self):
+        if (self.width == 0 or self.height == 0):
+            return ""
+        st = ""
+        for i in range(self.height):
+            st += (str(self.print_symbol) * self.width)
+            if i != self.__height - 1:
+                st += "\n"
+        return st
+
+    def __repr__(self):
+        """Print the rectangle using eval"""
+        return "Rectangle({}, {})".format(self.width, self.height)
+
+    def __del__(self):
+        """Print a message when the rectangle is deleted."""
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
