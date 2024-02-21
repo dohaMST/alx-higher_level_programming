75bd0219a6332397987fd12d1ff5603c6e9059ef
dohaMST <mastourdoha.dev001@gmail.com>
2024-02-20 16:50:29 -0800

    task7v2

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,30 +1,15 @@
 #!/usr/bin/python3
-"""load_from_json_file function"""
-import json
+"""Add all arguments to a Python list and save them to a file."""
 import sys
-import os
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-
-def main():
-    list_argv = []
-    for arg in sys.argv[1:]:
-        list_argv.append(arg)
-
-    file_name = 'add_item.json'
-
-    if not os.path.exists(file_name):
-        with open(file_name, 'w') as file:
-            file.write("[]")
-
-    existing_data = load_from_json_file(file_name)
-    existing_data.extend(list_argv)
-
-    save_to_json_file(existing_data, file_name)
-
-    load_from_json_file(file_name)
-
 
 if __name__ == "__main__":
-    main()
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
