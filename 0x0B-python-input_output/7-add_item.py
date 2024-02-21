0dc5ee97ba61b8a0ee5724e98a2df7e65ba11ff9
dohaMST <mastourdoha.dev001@gmail.com>
2024-02-20 16:36:11 -0800

    task7v2

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-"""defining the function"""
-import os
+"""load_from_json_file function"""
 import json
 import sys
+import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 def main():
-    listAr = []
+    list_argv = []
     for arg in sys.argv[1:]:
-        listAr.append(arg)
+        list_argv.append(arg)
 
-    f_name = 'add_item.json'
+    file_name = 'add_item.json'
 
-    if not os.path.exists(f_name):
-        with open(f_name, 'w') as file:
+    if not os.path.exists(file_name):
+        with open(file_name, 'w') as file:
             file.write("[]")
 
-    existing_data = load_from_json_file(f_name)
-    existing_data.extend(listAr)
+    existing_data = load_from_json_file(file_name)
+    existing_data.extend(list_argv)
 
-    save_to_json_file(existing_data, f_name)
+    save_to_json_file(existing_data, file_name)
 
-    load_from_json_file(f_name)
+    load_from_json_file(file_name)
 
 
 if __name__ == "__main__":
