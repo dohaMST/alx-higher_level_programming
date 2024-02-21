912d24c5fb5d827a5307d51486c1fb37e297b7a5
dohaMST <mastourdoha.dev001@gmail.com>
2024-02-20 16:10:28 -0800

    task

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""defining the function"""
 import json
 
 
 def from_json_string(my_str):
-    return json.loads(my_str)
+    """representing the function"""
+    data = json.loads(my_str)
+    return data
