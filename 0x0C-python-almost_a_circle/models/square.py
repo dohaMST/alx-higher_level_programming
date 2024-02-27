f073ee1f357c4bc893b05cd6c334fe4dad1ef844
dohaMST <mastourdoha.dev001@gmail.com>
2024-02-26 17:16:24 -0800

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-'''Module for Square class.'''
+'''defining the class'''
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    '''A Square class.'''
+    '''representing the class'''
 
     def __init__(self, size, x=0, y=0, id=None):
-        '''Constructor.'''
+        '''the constructor'''
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        '''Returns string info about this square.'''
+        '''representing the class: string info about this class'''
         return '[{}] ({}) {}/{} - {}'.\
             format(type(self).__name__, self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
-        '''Size of this square.'''
+        '''get the size'''
         return self.width
 
     @size.setter
@@ -26,7 +26,7 @@ class Square(Rectangle):
         self.height = value
 
     def __update(self, id=None, size=None, x=None, y=None):
-        '''Internal method that updates instance attributes via */**args.'''
+        '''updating instance attributes via */**args.'''
         if id is not None:
             self.id = id
         if size is not None:
@@ -37,13 +37,13 @@ class Square(Rectangle):
             self.y = y
 
     def update(self, *args, **kwargs):
-        '''Updates instance attributes via no-keyword & keyword args.'''
+        '''updating the instances'''
         if args:
             self.__update(*args)
         elif kwargs:
             self.__update(**kwargs)
 
     def to_dictionary(self):
-        '''Returns dictionary representation of this class.'''
+        '''dict representation for the class'''
         return {"id": self.id, "size": self.width,
                 "x": self.x, "y": self.y}
